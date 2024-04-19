b13383a8ed5b9c0c1f647e913acd87c3111e777e
sangwonyou <tkddnjs231@naver.com>
2024-04-19 14:15:11 +0900

    fix: react query => tank query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import type { AppProps } from "next/app";
-import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { useState } from "react";
+import React, { lazy, useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
 
 import "semantic-ui-css/semantic.min.css";
@@ -11,31 +9,47 @@ import "react-responsive-modal/styles.css";
 import PageFrame from "@src/component/common/pageFrame/PageFrame";
 import { Toaster } from "react-hot-toast";
 import { initMSW } from "@src/mocks";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 initMSW();
 
+const ReactQueryDevtoolsProduction = lazy(() =>
+  import("@tanstack/react-query-devtools/build/modern/production.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    })
+  )
+);
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            cacheTime: 1000 * 60 * 5, // 5 minute
-            retry: 0,
-          },
-        },
-      })
-  );
+  const [showDevtools, setShowDevtools] = useState(false);
+
+  useEffect(() => {
+    // @ts-expect-error
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <PageFrame>
-          <Component {...pageProps} />
-        </PageFrame>
-      </Hydrate>
+      <PageFrame>
+        <Component {...pageProps} />
+      </PageFrame>
       <ReactQueryDevtools initialIsOpen={true} />
       <Toaster position="bottom-center" reverseOrder={false} />
+      {showDevtools && (
+        <React.Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </React.Suspense>
+      )}
     </QueryClientProvider>
   );
 }
